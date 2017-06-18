f59c1b38f741164cd8ea0f3b0332ba8cfe9bd1b4
Marcin Karpinski <marcin.karpinski@hexagongeospatial.com>
2017-06-18 16:53:40 +0200

    Corrected module building

--- gruntfile.js
+++ gruntfile.js
@@ -1,53 +1,38 @@
 module.exports = function(grunt) {    
     grunt.initConfig({
-        requirejs: {
-            dev: {
+        umd: {
+            build: {
                 options: {
-                    name: "src/main",
-                    mainConfigFile: "src/main.js",
-                    out: "build/facebook.explorer.js",
-                    paths: {
-                        "facebook": "//connect.facebook.net/en_US/sdk.js?version=v2.9",
-                        "bluebird": "empty:",
-                        "lodash": "empty:",
-                        "jquery": "empty:"
-                    },
-                    shim: {
-                        "facebook": { exports: "FB" }
-                    },
-                    optimize: "none"
-                }
-            },
-            dist: {
-                options: {
-                    name: "src/main",
-                    mainConfigFile: "src/main.js",
-                    out: "build/facebook.explorer.min.js",
-                    paths: {
-                        "facebook": "//connect.facebook.net/en_US/sdk.js?version=v2.9",
-                        "bluebird": "empty:",
-                        "lodash": "empty:",
-                        "jquery": "empty:"
-                    },
-                    shim: {
-                        "facebook": { exports: "FB" }
-                    },
-                    optimize: "uglify2",
-                    uglify2: {
-                        compress: {
-                            dead_code: true,
-                            conditionals: true,
-                            evaluate: true,
-                            unused: true,
-                            join_vars: true,
-                            drop_console: true,
-                            drop_debugger: true,
-                            comparisons: true,
-                            booleans: true,
-                            loops: true,
-                            if_return: true
-                        }
+                    src: "src/main.js",
+                    dest: "build/facebook.explorer.js",
+                    globalAlias: "FBExplorer",
+                    deps: {
+                        "default": [
+                            { "bluebird": "Promise" },
+                            { "facebook": "FB" },
+                            { "lodash": "_" }
+                        ]
                     }
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: [{
+                    "build/facebook.explorer.min.js": ["build/facebook.explorer.js"]
+                }],
+                compress: {
+                    dead_code: true,
+                    conditionals: true,
+                    evaluate: true,
+                    unused: true,
+                    join_vars: true,
+                    drop_console: true,
+                    drop_debugger: true,
+                    comparisons: true,
+                    booleans: true,
+                    loops: true,
+                    if_return: true
                 }
             }
         },
@@ -71,13 +56,13 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks("grunt-contrib-requirejs");
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-downloadfile');
+    grunt.loadNpmTasks('grunt-umd');
     grunt.renameTask("clean", "_clean");
 
     var cleanTask = ["_clean"];
-    var buildTask = ["_clean", "downloadfile", "requirejs"];
+    var buildTask = ["_clean", "downloadfile", "umd", "uglify"];
     
     grunt.registerTask("default", buildTask);
     grunt.registerTask("clean", cleanTask);
